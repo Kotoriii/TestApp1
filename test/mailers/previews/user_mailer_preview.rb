d8504eca7c85a1149607a11ff682ed7028470e01
Christian M <cmackm@hotmail.com>
2016-03-14 16:45:58 -0600

    Added rspec and a testing branch

--- test/mailers/previews/user_mailer_preview.rb
+++
@@ -1,6 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
-class UserMailerPreview < ActionMailer::Preview
-	def contact_form
-		UserMailer.contact_form("test@test.com", "Testerooni", "Hello World!")
-	end
-end
