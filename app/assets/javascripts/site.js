89eb5d56049dd75a3cd49696cf101792edbcf494
Christian M <cmackm@hotmail.com>
2016-03-29 18:36:42 -0600

    Added AJAX functionality to new comments

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,9 +1,12 @@
-$(document).on('ready page:load', function(){
+function raty(){
 	$('.rating').raty( {path: '/assets', scoreName: 'comment[rating]'});
 	$('.rated').raty({ path: '/assets', readOnly: true, 
 		score: function(){
 			return $(this).attr('data-score');
 		}
 	});
+	}
+$(document).on('ready page:load', function(){
+	raty();
 	$('.img-zoom').elevateZoom();
 });
